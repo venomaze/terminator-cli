745ec6f37f1e699de05b43ab7c08da362ddd3448
venomaze <venomaze@protonmail.com>
2020-09-07 08:30:32 -0700

    Add overwrite utility

---
+++ src/lib/overwrite.js
@@ -0,0 +1,36 @@
+const fs = require('fs-extra');
+const crypto = require('crypto');
+const util = require('util');
+
+const randomBytes = util.promisify(crypto.randomBytes);
+
+module.exports = async (filePath, writeCallback) => {
+  const stat = await fs.stat(filePath);
+  const fileSize = stat.size;
+  const bufferLength = 10240;
+
+  let file = await fs.open(filePath, 'w');
+  let position = 0;
+
+  while (position < fileSize) {
+    const requiredLength = Math.min(bufferLength, fileSize - position);
+    const buffer = await randomBytes(requiredLength);
+    const { bytesWrtitten } = await fs.write(
+      file,
+      buffer,
+      0,
+      requiredLength,
+      position
+    );
+
+    if (bytesWrtitten !== requiredLength) {
+      throw new Error('Lost data while overwriting.');
+    }
+
+    writeCallback(requiredLength);
+    position += requiredLength;
+  }
+
+  await fs.close(file);
+  file = null;
+};
